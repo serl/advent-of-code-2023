7c67a8a2f60fb2dbe568e7cabee97854e1117bfc
Sergio Livi <me@serl.it>
2023-12-04 18:58:02 +0100

    Day 4 Part 2

--- 04/__main__.py
+++ 04/__main__.py
@@ -1,5 +1,6 @@
 import re
 import unittest
+from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Self
@@ -28,10 +29,13 @@ class PartOne:
             )
 
         @property
+        def matching(self) -> int:
+            return len(self.winning.intersection(self.having))
+
+        @property
         def points(self) -> int:
-            count_having = len(self.winning.intersection(self.having))
-            if count_having > 0:
-                return 2 ** (count_having - 1)
+            if self.matching > 0:
+                return 2 ** (self.matching - 1)
             return 0
 
     def __init__(self, file_name: str) -> None:
@@ -58,7 +62,7 @@ class PartOneTestCase(unittest.TestCase):
             winning={41, 48, 83, 86, 17},
             having={83, 86, 6, 31, 17, 9, 48, 53},
         )
-
+        self.assertEqual(winning_card.matching, 4)
         self.assertEqual(winning_card.points, 8)
 
         losing_card = PartOne.Card(
@@ -66,6 +70,7 @@ class PartOneTestCase(unittest.TestCase):
             winning={31, 18, 13, 56, 72},
             having={74, 77, 10, 23, 35, 67, 36, 11},
         )
+        self.assertEqual(losing_card.matching, 0)
         self.assertEqual(losing_card.points, 0)
 
     def test_sample(self):
@@ -75,6 +80,33 @@ class PartOneTestCase(unittest.TestCase):
     def test_solve(self):
         found_solution = PartOne("input.txt").solve()
         self.assertEqual(found_solution, 26218)
+
+
+class PartTwo:
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def solve(self) -> int:
+        lines = read_file(self.file_name)
+        cards = [PartOne.Card.from_line(line) for line in lines]
+
+        card_quantities = defaultdict(lambda: 1)
+        for card in cards:
+            current_copies = card_quantities[card.id]
+            for id_delta in range(card.matching):
+                card_quantities[card.id + id_delta + 1] += current_copies
+
+        return sum(card_quantities.values())
+
+
+class PartTwoTestCase(unittest.TestCase):
+    def test_sample(self):
+        found_solution = PartTwo("sample.txt").solve()
+        self.assertEqual(found_solution, 30)
+
+    def test_solve(self):
+        found_solution = PartTwo("input.txt").solve()
+        self.assertEqual(found_solution, 9997537)
 
 
 if __name__ == "__main__":
