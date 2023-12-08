464ef2bdcd70713e4a47e4918e60f33159384775
Sergio Livi <me@serl.it>
2023-12-08 18:49:59 +0100

    Day 8 Part 2

--- 08/__main__.py
+++ 08/__main__.py
@@ -2,6 +2,7 @@ import re
 import unittest
 from pathlib import Path
 from typing import Self
+import math
 
 
 def read_file(file_name: str) -> list[str]:
@@ -14,7 +15,7 @@ class Graph(dict[str, dict[str, str]]):
     def from_lines(cls, lines: list[str]) -> Self:
         graph = cls()
         for line in lines:
-            match = re.fullmatch(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)", line)
+            match = re.fullmatch(r"([\w]{3}) = \(([\w]{3}), ([\w]{3})\)", line)
             assert match
             graph[match[1]] = {"L": match[2], "R": match[3]}
         return graph
@@ -69,6 +70,36 @@ class PartOneTestCase(unittest.TestCase):
     def test_solve(self):
         found_solution = PartOne("input.txt").solve()
         self.assertEqual(found_solution, 12737)
+
+
+class PartTwo(PartOne):
+    def solve(self) -> int:
+        instructions, graph = self.parse_input()
+
+        steps = 1
+        current_nodes = [graph[node] for node in graph.keys() if node.endswith("A")]
+        minimum_steps = [0] * len(current_nodes)
+        for which in infinite_instructions(instructions):
+            next_nodes = [node[which] for node in current_nodes]
+            for i, node in enumerate(next_nodes):
+                if minimum_steps[i] == 0 and node.endswith("Z"):
+                    minimum_steps[i] = steps
+            if all(s > 0 for s in minimum_steps):
+                break
+            current_nodes = [graph[next_node] for next_node in next_nodes]
+            steps += 1
+
+        return math.lcm(*minimum_steps)
+
+
+class PartTwoTestCase(unittest.TestCase):
+    def test_sample_3(self):
+        found_solution = PartTwo("sample_3.txt").solve()
+        self.assertEqual(found_solution, 6)
+
+    def test_solve(self):
+        found_solution = PartTwo("input.txt").solve()
+        self.assertEqual(found_solution, 9064949303801)
 
 
 if __name__ == "__main__":
